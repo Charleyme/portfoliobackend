ddbf2c5cde4dd25147e154039eced52235dc03f8
Charleyme <charlesjiwueze826@gmail.com>
2025-11-30 12:54:50 +0100

    commitedmongo

--- config/db.js
+++ config/db.js
@@ -4,6 +4,7 @@ const connectDB = async () =>{
     try{
         const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
+        console.log("MONGO_URI:", process.env.MONGO_URI);
 
     }catch(error){
         console.error("Error connecting to database:", error);
